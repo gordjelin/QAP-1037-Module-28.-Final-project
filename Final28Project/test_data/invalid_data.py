15e7cac6b0944c7e157d4cdaaa07b458394b6360
Anton Erofeev <69521655+gordjelin@users.noreply.github.com>
2023-01-15 19:51:16 +0200

    Add files via upload

---
+++ Final28Project/test_data/invalid_data.py
@@ -0,0 +1,12 @@
+# Переменные с НЕвалидными  данными для тестов
+
+incorrect_names = ['а', 'z', 'аб', 'AZ', '12345']
+incorrect_surnames = ['а', 'z', 'аб', 'AZ', '12345']
+incorrect_phones = ['812345', '0000000000', '-20', '2.55', '2541263355225545224525']
+incorrect_emails = ['tests@tests.com', 'tests2test@tests.com']
+incorrect_passwords = ['-4_68q', 'роывhk', '@@@@777', '---_=+', 'z']
+incorrect_logins = ['sadsdasdasd', '+71213252111']
+incorrect_phone = '71213252111'
+incorrect_email = 'assdsddas@bademmailrasras.cam'
+incorrect_login_yandex = 'gr34583fdf3@ya.rus'
+incorrect_pass = 'sdad2312asdas'
---
+++ Final28Project/test_data/valid_data.py
@@ -0,0 +1,7 @@
+# Переменные с валидными  данными для тестов
+
+correct_logins = ['gordjelin14@gmail.com', '79097771817']
+correct_pass = 'BiomorF5422'
+
+agreement_url = 'https://b2c.passport.rt.ru/sso-static/agreement/agreement.html'
+agreement_title = 'Публичная оферта о заключении Пользовательского соглашения на использование Сервиса «Ростелеком ID»'
